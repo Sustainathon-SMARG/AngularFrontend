a33ecc5f77dd7055c1c5588a0ee99f1b754e2b36
Ghareeb Falazi <ghareeb.falazi@hotmail.com>
2023-10-31 13:59:53 +0100

    work

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,22 +10,37 @@ import {checkBudgets} from "@angular-devkit/build-angular/src/utils/bundle-calcu
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
   HAPPY_MESSAGE = 'I am so happy now!';
   SAD_MESSAGE = 'I woke up sad today...';
+  SAD_IMAGE: string = "https://www.acomaanimalclinictucson.com/wp-content/uploads/2020/04/AdobeStock_265968181-scaled.jpeg";
+  HAPPY_IMAGE: string = "https://www.gather-cowork.com/wp-content/uploads/2021/06/happy-pup-1.png";
   petName: string = 'dot';
   message = this.SAD_MESSAGE;
+  image: string = this.SAD_IMAGE;
   challenges: ChallengeModel[] = new Array<ChallengeModel>;
 
   constructor(private userService: UserService,
-              private sensorService: SensorService,
               private challengesService: ChallengesService) {
   }
 
   ngOnInit(): void {
     this.petName = `${this.userService.userName}'s dot`;
     this.challenges = this.challengesService.getChallenges();
+    this.challengesService.subscribeToChangedChallenges().subscribe(challenges => {
+      let completedChallenges = challenges?.filter(ch => ch.accomplished);
+      if (completedChallenges != undefined) {
+        this.handleChallengesAccomplished(completedChallenges);
+      }
+    });
 
+  }
+
+  handleChallengesAccomplished(challenges: ChallengeModel[]) {
+    //console.log(challenges);
+    this.image = this.HAPPY_IMAGE;
+    this.message = this.HAPPY_MESSAGE;
+    this.challenges = this.challengesService.getChallenges();
   }
 
 }
